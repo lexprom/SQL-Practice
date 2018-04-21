b4a8a2006367e0b79fc0838aed5efc94757a44ce
Пак Александр Сергеевич <PakAS@main.sgu.ru>
2018-04-21 11:29:54 +0400

    added some select scripts

--- SQL Select.sql
+++ SQL Select.sql
@@ -35,3 +35,43 @@ WHERE EXISTS (SELECT Название_кухни FROM Cuisine WHERE SUBSTRING(Название_кухни,
 /*SELECT Название_кухни
 FROM Cuisine
 WHERE (SUBSTRING(Название_кухни, 1, DATALENGTH(Название_кухни)))  IN (N'Русская')*/
+
+--ALL
+/*SELECT Название_кухни
+FROM Cuisine
+WHERE ID_кухня > ALL (SELECT ID_кухня FROM Cuisine WHERE ID_кухня > 1)*/
+
+
+--ANY/SOME
+/*SELECT *
+FROM Dishes
+WHERE ID_посуды = SOME/ANY (SELECT ID_посуды FROM Dishes)*/
+
+--BETWEEN
+/*SELECT * 
+FROM Dishes
+WHERE ID_посуды BETWEEN 0 AND 2*/
+
+--LIKE
+/*SELECT *
+FROM Dishes
+WHERE Название LIKE N'%р%'*/
+
+--CASE
+/*SELECT *,
+CASE
+WHEN ID_ингредиента is NULL
+THEN 'Нету в наличии'
+END
+FROM Ingredient
+RIGHT OUTER JOIN Category ON Ingredient.ID_ингредиента = Category.ID_категории*/
+
+--CAST
+/*SELECT CAST(AVG(Калорийность) AS VARCHAR(20))
+FROM Ingredient*/
+
+--CONVERT
+/*SELECT CONVERT(int,Калорийность) AS INT
+FROM Ingredient*/
+
+--ISNULL,NULLIF
