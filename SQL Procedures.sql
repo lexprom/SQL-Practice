d113c62ae23112bd505aff7bba42af80c55c9f09
Lex Park <lexprom009@gmail.com>
2018-05-18 21:29:26 +0400

    added views,procedures(besides last)

---
+++ SQL Procedures.sql
@@ -0,0 +1,50 @@
+﻿--Процедура с входным параметром.
+
+/*
+CREATE PROC sp_proc1 @num INT
+AS
+SELECT ID_raiting,Rating.ID_user
+FROM [User] INNER JOIN Rating 
+ON ID_raiting=[User].ID_user
+WHERE [User].ID_user = @num
+
+EXEC sp_proc1 12
+
+DROP PROC sp_proc1
+*/
+
+--Процедура с входными и выходными параметрами.
+/*
+CREATE PROC sp_proc2 @name varchar(50), @sum decimal OUTPUT
+AS
+SELECT @sum = SUM(Price) FROM Ingredient INNER JOIN Recipe ON @Reci_name  = @name
+AND ID_ingredient = ID_recipe
+
+
+DECLARE @st decimal
+EXEC sp_proc2 'Borsh',@st OUTPUT
+SELECT @st
+
+DROP PROC sp_proc2
+*/
+
+--процедурa c входными параметрами и циклом
+--вывод клиентов по размерам
+/*
+CREATE PROCEDURE sp_proc3 (@i INT = 1)
+AS
+
+WHILE (@i<3) 
+BEGIN
+SELECT Ingredient.ID_ingredient,Ingredient.Name,Count FROM Composition
+INNER JOIN Ingredient
+ON Ingredient.ID_ingredient = ID_composition
+WHERE Ingredient.ID_ingredient=@i;
+SET @i=@i+1;
+END 
+
+
+EXEC sp_proc3 1
+
+DROP PROC sp_proc3
+*/
---
+++ SQL View.sql
@@ -0,0 +1,70 @@
+﻿USE Recipe
+GO
+
+--WITH ENCYPTION
+/*
+CREATE VIEW FirstView WITH ENCRYPTION
+AS
+SELECT U.ID_user as 'User ID', U.First_name as 'First Name', U.Last_name as 'Last Name'
+FROM [User] as U
+JOIN Rating as R ON U.ID_user=R.ID_user;
+GO
+
+EXEC sp_helptext 'FirstView'
+GO
+
+print('FirstView')
+SET statistics io on
+SET statistics time on
+
+SELECT * FROM FirstView
+
+print('FirstView')
+
+DROP VIEW FirstView
+GO
+*/
+
+--ON UPDATE
+/*CREATE VIEW View_INSERT
+AS
+SELECT U.First_name, U.Last_name
+FROM [User] as U
+WITH CHECK OPTION
+GO
+
+
+INSERT INTO [User]
+VALUES
+(996, 'Alex','Park')
+GO
+
+/*SELECT First_name FROM [User] WHERE ID_user = 996*/
+DROP VIEW View_INSERT 
+*/
+
+
+--INDEX
+GO
+SET NUMERIC_ROUNDABORT OFF;
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL,
+ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+
+GO
+CREATE VIEW View_3 WITH SCHEMABINDING 
+AS
+SELECT ID_cuisine,Cuisine_name
+FROM Cuisine
+WHERE ID_cuisine BETWEEN 1 AND 2
+
+GO
+CREATE UNIQUE CLUSTERED INDEX Index_On_View ON View_3 (ID_cuisine) 
+
+SET STATISTICS TIME ON
+
+SELECT * FROM View_3
+
+SET STATISTICS TIME OFF
+GO
+DROP VIEW View_3
